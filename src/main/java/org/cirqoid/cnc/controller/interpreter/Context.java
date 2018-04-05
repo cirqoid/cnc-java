e77c4be66e923f8e045cbb11d69e3a95e73b1da6
Simon Salykov <simon.salykov@gmail.com>
2018-04-05 10:35:24 +0200

    Modifications in files moved here from cirqwizard since CQ-217 was branched from new-firmware-dev

--- src/main/java/org/cirqoid/cnc/controller/interpreter/Context.java
+++ src/main/java/org/cirqoid/cnc/controller/interpreter/Context.java
@@ -28,7 +28,6 @@ public class Context implements Cloneable
     private int offsets[][] = new int[ApplicationConstants.WCS_COUNT][ApplicationConstants.MAX_AXES_COUNT + 1];
     private int currentPosition[] = new int[ApplicationConstants.MAX_AXES_COUNT];
     private Plane plane = Plane.XY;
-    private int arcCenterOffset[] = new int[3];
     private Integer feed = null;
     private Integer speed = null;
     private int relaysStatus;
@@ -86,16 +85,6 @@ public class Context implements Cloneable
     public void setPlane(Plane plane)
     {
         this.plane = plane;
-    }
-
-    public int getArcCenterOffset(int axis)
-    {
-        return arcCenterOffset[axis];
-    }
-
-    public void setArcCenterOffset(int axis, int offset)
-    {
-        arcCenterOffset[axis] = offset;
     }
 
     public Integer getFeed()
